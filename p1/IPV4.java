30c8636167edf26d935c112c386a411f964a6e40
Graeme Hollingsworth <gdhollingsworth93@gmail.com>
2019-10-07 11:44:46 -0400

    WIP

--- p1/IPV4.java
+++ p1/IPV4.java
@@ -2,16 +2,54 @@ package p1;
 
 public class IPV4 {
 
+    private String givenIP;
+    private String netmask;
+
     public IPV4(String givenIP, String netmask) {
-        /*
-        Checking the classful IP address
-        We do this by converting the number to binary, then checking to see if that number is in range of
-        the class.
-         */
 
-        String tempOctet = givenIP.substring(0, 3);
-        int firstOctet = Integer.parseInt(tempOctet);
+        this.givenIP = givenIP;
+        this.netmask = netmask;
 
-        
     }
+
+    public IPV4(String givenIP) {
+
+        this.givenIP = givenIP;
+    }
+
+    public static int[] convertToBinary(String[] givenIP)
+    {
+        int address[] = new int[32];
+        int oct1, oct2, oct3, oct4, x, remainder;
+
+
+
+    }
+
+
+
+    public static String findClass(String givenIP)
+    {
+       int index = givenIP.indexOf(".");
+       String tempOct = givenIP.substring(0,index);
+        int firstOct = Integer.parseInt(tempOct);
+
+        if (firstOct >= 1 && firstOct <= 127)
+            return "A";
+
+        else if (firstOct >= 128 && firstOct <= 191)
+            return "B";
+
+        else if (firstOct >= 192 && firstOct <= 223)
+            return "C";
+
+        else if (firstOct >= 224 && firstOct <= 239)
+            return "D";
+
+        else {
+                return "E";
+            }
+
+    }
+
 }
